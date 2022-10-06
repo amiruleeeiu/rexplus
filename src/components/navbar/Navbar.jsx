53d3a131af661774470c62da4471cf328c95b02c
Amirul islam <amiruleeeiu15@gmail.com>
2022-10-06 16:20:45 +0600

    add sidebar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -39,12 +39,14 @@ const NAV_LINK=[
 const Navbar = () => {
 
     const [offset, setOffset] = useState(0);
+    const[sidebar,setSidebar]=useState(false);
 
     useEffect(()=>{
         const onScroll=()=>setOffset(window.pageYOffset);
         window.addEventListener('scroll', onScroll, { passive: true });
     },[])
 
+    console.log(sidebar);
 
     return (
         <div className={`${offset>0 ? 'scrollNav' : 'unScrollNav'} nav`}>
@@ -56,8 +58,21 @@ const Navbar = () => {
                         NAV_LINK.map((nav,index)=><NavLink to={nav.path} key={index} end>{nav.display}</NavLink>)
                     }
             </div>
-            <div className="nav_logo">
-                <FaAlignJustify/>
+            <div className="sidebar">
+                    <div className={`${sidebar ? 'activeSidebar' : null} sidebar_items`}>
+                        <ul>
+                        {
+                            NAV_LINK.map((nav,index)=><li><NavLink to={nav.path} key={index} end>{nav.display}</NavLink></li>)
+                        }
+                        </ul>
+                    </div>
+            </div>
+            <div className="nav_logo" onClick={()=>setSidebar((prevState=>!prevState))}>
+                {
+                    sidebar ? <i class="ri-subscript"></i> : <i class="ri-list-check"></i>
+                }
+                
+                
             </div>
         </div>
     );
